b5533c6e693941f12e42e900aa28144d80080dfe
Kyle Corbin <kylecorbin26@gmail.com>
2019-12-05 13:48:13 -0500

    test

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ server.get('/', (req, res) => {
   const message = process.env.MSG || "hello world";
   Shoutouts.find()
   .then(shoutouts => {
-    res.status(200).json({message, shoutout});
+    res.status(200).json({message, shoutouts});
   })
   .catch (error => {
     console.error('\nERROR', error);
